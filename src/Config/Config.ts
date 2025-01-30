65878c78c758674ac4a25729f08e67c92320e11b
DevarajP2713 <devarajupm7@gmail,com>
2025-01-30 10:59:25 +0530

    Deva changes 30/01/2025 10:58 p1

--- src/Config/Config.ts
+++ src/Config/Config.ts
@@ -3,11 +3,11 @@ import { IADGroupID, IListNames, ISiteURL } from "../Interface/Interface";
 
 export namespace GCongfig {
   export const ListName: IListNames = {
-    CheckpointConfig: "CheckpointConfig",
-    Department: "Department",
-    EmployeeOnboarding: "EmployeeOnboarding",
-    EmployeeResponse: "EmployeeResponse",
-    Forms: "Forms",
+    CheckpointConfig: "Onboarding_CheckpointConfig",
+    Department: "Departments",
+    EmployeeOnboarding: "Onboarding_EmployeeOnboarding",
+    EmployeeResponse: "Onboarding_EmployeeResponse",
+    Forms: "Onboarding_Forms",
   };
 
   export const SiteURL: ISiteURL = {
@@ -15,10 +15,10 @@ export namespace GCongfig {
     // siteUrl: `${window.location.origin}/sites/LogiiDev`,
 
     // UAT
-    siteUrl: `${window.location.origin}/sites/Automation-Dev`,
+    // siteUrl: `${window.location.origin}/sites/Automation-Dev`,
 
     // Prod
-    // siteUrl: ""
+    siteUrl: `${window.location.origin}`,
   };
 
   export const ADGroupID: IADGroupID = {
@@ -27,11 +27,11 @@ export namespace GCongfig {
     // HRPersonID: "f092b7ad-ec31-478c-9225-a87fa73d65d1",
 
     // UAT
-    HRDirectorID: "738add66-3c24-47a5-bfac-284bf7013f2c",
-    HRPersonID: "0a446dc3-dd61-4d50-826c-f20d1b6621d5",
+    // HRDirectorID: "738add66-3c24-47a5-bfac-284bf7013f2c",
+    // HRPersonID: "0a446dc3-dd61-4d50-826c-f20d1b6621d5",
 
     // Prod
-    // HRDirectorID: "",
-    // HRPersonID: "",
+    HRDirectorID: "0b2b0a19-a3cc-4143-b82e-69f92eb3066f",
+    HRPersonID: "ee33c03e-820c-4673-a733-830f3fb4faf1",
   };
 }
